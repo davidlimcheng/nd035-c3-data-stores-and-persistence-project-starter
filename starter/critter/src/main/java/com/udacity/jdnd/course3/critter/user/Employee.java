61237938c896215934e62ab824d2c3180563963b
David Lim Cheng <davidlimcheng@gmail.com>
2021-02-05 23:49:22 -0800

    add database initialization properties to application.properties

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Employee.java
@@ -0,0 +1,10 @@
+//package com.udacity.jdnd.course3.critter.user;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table
+//public class Employee {
+//
+//}
