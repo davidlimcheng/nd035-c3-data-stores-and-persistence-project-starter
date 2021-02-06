ae3703c7a0e7b920f4440b5a1703d7fd0a7b762e
David Lim Cheng <davidlimcheng@gmail.com>
2021-02-06 14:33:16 -0800

    Create Pet, Employee, Customer, Schedule entities

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
@@ -0,0 +1,83 @@
+package com.udacity.jdnd.course3.critter.pet;
+
+import com.udacity.jdnd.course3.critter.schedule.Schedule;
+import com.udacity.jdnd.course3.critter.user.Customer;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table
+public class Pet {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private PetType petType;
+    private String name;
+    private String notes;
+    private LocalDateTime birthDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Customer customer;
+
+    @ManyToMany
+    private List<Schedule> schedules;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Schedule> getSchedules() {
+        return schedules;
+    }
+
+    public void setSchedules(List<Schedule> schedules) {
+        this.schedules = schedules;
+    }
+}
